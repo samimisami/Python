2ad403ee9d65b59f3e5c59222be4875ac9d8c300
Ekmen <EKMENBX@abbvienet.com>
2024-10-08 14:20:36 +0300

    v1.1

--- Exchange Rates/extraction2excel.py
+++ Exchange Rates/extraction2excel.py
@@ -5,5 +5,7 @@ def extraction2excel(dataset, extraction_name):
         df = pd.DataFrame(dataset)
         # print(df)
         
+        df['buying_rate'].ffill(inplace=True)
+
         # Save to Excel
         df.to_excel(extraction_name+'_EUR-TRY.xlsx', index=False)
